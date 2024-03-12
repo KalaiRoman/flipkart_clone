5a59b8b47a2b48730fb8b0835199eb9b968fc65b
kalai <kalai@cdp360.com>
2024-03-12 22:34:20 +0530

    new folder structures

--- Server.js
+++ Server.js
@@ -1,1 +1,16 @@
-console.log("kalai")
+import dotenv from 'dotenv';
+
+import express from 'express';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server Running ${process.env.PORT}`)
+})
+
+
+console.log("kalia")
