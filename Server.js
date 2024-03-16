50d2dcc5c87c5962093d8fe30968aaa56bc1b247
kalai <kalai@cdp360.com>
2024-03-16 14:32:10 +0530

    cors

--- Server.js
+++ Server.js
@@ -15,26 +15,26 @@ const app = express();
 app.use(express.json());
 
 app.use(bodyparser.urlencoded({
+    parameterLimit: 100000,
     limit: '50mb',
     extended: true,
+    type: 'application/json'
 }));
 
-// var whitelist = ['http://localhost:3000/', 'https://admin-flipkart.onrender.com/', 'https://seller-flipkart.onrender.com/', 'https://flipkart-pvag.onrender.com/']
-// var corsOptionsDelegate = function (req, callback) {
-//     var corsOptions;
-//     if (whitelist.indexOf(req.header('Origin')) !== -1) {
-//         corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-//     } else {
-//         corsOptions = { origin: false } // disable CORS for this request
-//     }
-//     callback(null, corsOptions) // callback expects two parameters: error and options
-// }
-
-app.use(cors("*"));
+var whitelist = ['http://localhost:3000', 'https://admin-flipkart.onrender.com/', 'https://seller-flipkart.onrender.com/', 'https://flipkart-pvag.onrender.com/']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+}
 // helmet secure
 app.use(helmet());
 // routing apis
-app.use("/flip/ecommerce", router);
+app.use("/flip/ecommerce", cors(corsOptionsDelegate), router);
 // listing port in db server
 
 // error middleware
