24499e88662dbf284ac14c62792e67c5c2f02bc1
kalai <kalai@cdp360.com>
2024-03-25 10:05:39 +0530

    bug fixing

--- Controlls/auth/seller/Seller_controll.js
+++ Controlls/auth/seller/Seller_controll.js
@@ -44,7 +44,7 @@ export const createSeller = async (req, res) => {
         const token = jwt.sign({ _id: newSeller._id }, process.env.TOKEN, { expiresIn: "10d" });
         return res.status(201).json({ message: "Seller Registered Successfully", user: newSeller, token });
     } catch (error) {
-        
+
         return res.status(500).json({ message: "Internal Server Error" });
     }
 }
@@ -132,6 +132,7 @@ export const userLogin = async (req, res) => {
 
 
     } catch (error) {
+        console.log(error)
         return res.status(500).json({ message: "Internal Server Error" });
     }
 }
